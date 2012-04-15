b8f7460c5024289a0fce3cea8cd1c840f4130652
Edgars Beigarts <edgars.beigarts@makit.lv>
2012-04-15 04:34:06 +0300

    Smooth scroll

--- public/javascripts/vespaspiets.js
+++ public/javascripts/vespaspiets.js
@@ -9,4 +9,9 @@ $(function() {
     var offset = ($(document.body).scrollTop() - 1230) * 1.5;
     $("#flying_vespa").css("background-position", offset + 13 + "px 0");
   });
+
+  $("#nav a").click(function(event) {
+    event.preventDefault();
+    $($(this).attr("href")).ScrollTo({ duration: 800 });
+  });
 });
