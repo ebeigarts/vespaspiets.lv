53119cc53d02133ab92c37fd2556034280a862b6
Edgars Beigarts <edgars.beigarts@makit.lv>
2012-05-10 22:54:09 +0300

    Poster

--- app.rb
+++ app.rb
@@ -8,11 +8,13 @@ module VespaSpiets
     get "/" do
       # @oauth = Koala::Facebook::OAuth.new(348126495249042, "a26aa29841f5f43b5c32cb663184c966")
       # @access_token = @oauth.get_app_access_token
-      @access_token = "348126495249042|57jpAVi0NRRcOBq4eSdGH8luzCk"
-      @graph = Koala::Facebook::API.new(@access_token)
-      @feed = @graph.get_connections("107178829336751", "feed").select do |entry|
-        entry["type"] == "photo" && !entry.key?("application")
-      end.first(3)
+      # @access_token = "348126495249042|57jpAVi0NRRcOBq4eSdGH8luzCk"
+      # @graph = Koala::Facebook::API.new(@access_token)
+      # @feed = @graph.get_connections("107178829336751", "feed").select do |entry|
+      #   # entry["type"] == "photo" #&& !entry.key?("application")
+      #   true
+      # end #.first(3)
+      @feed = []
       erb :index
     end
   end
